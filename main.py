f6932e61cb9e20153651bbd178153fbfa72f82f5
rrosta <rrosta@yahoo.com>
2022-11-11 20:26:11 +0330

    Add hello world translations

--- main.py
+++ main.py
@@ -99,6 +99,42 @@ print("             _ _ \n"
       "/ / (_) | (_| | | (_| | (__ \n"
       "/___\___/ \__,_|_|\__,_|\___|\n")
 
+
+hello_world_translations = {'Arabic': 'مرحبا بالعالم!',
+                            'Chinese': '你好世界！',
+                            'English': 'Hello World!',
+                            'French': 'Bonjour monde!',
+                            'German': 'Hallo Welt!',
+                            'Persian': 'سلام دنیا!',
+                            'Russian': 'Привет мир!'}
+
+while True:
+    print("What language do you know?")
+    print("Option:\n"
+          "- Arabic \n"
+          "- Chinese\n"
+          "- English\n"
+          "- French\n"
+          "- German\n"
+          "- Persian\n"
+          "- Russian")
+    language = input()
+    if language in hello_world_translations.keys():
+        print(hello_world_translations[language])
+        break
+    else:
+        print("We don't support this languange yet\n"
+              "\n"
+              "　　　　　🌸＞＿＿ フ\n"
+              "　　　　　| 　_　 _ l\n"
+              "　 　　　／` ミ＿xノ\n"
+              "　　 　 /　　　 　 |\n"
+              "　　　 /　 ヽ　　 ﾉ\n"
+              "　 　 │　　|　|　|\n"
+              "　／￣|　　 |　|　|\n"
+              "　| (￣ヽ＿_ヽ_)__)\n"
+              "　＼二つ")
+
 while True:
     year = int(input("Input your birth year: "))
     if (year - 2000) % 12 == 0:
