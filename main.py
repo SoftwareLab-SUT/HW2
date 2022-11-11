09962e81e0cc83b74604572a83723026214dc537
rrosta <rrosta@yahoo.com>
2022-11-11 20:18:27 +0330

    Add hello world translations

--- main.py
+++ main.py
@@ -6,6 +6,50 @@ songs = {'Sad': 'モラトリアム by Moratorium',
         'In Love': 'Love Story by Taylor Swift',
         'Lazy': 'Ain\'t your mama by Jennifer Lopez'}
 
+hello_world_translations = {'Arabic': 'مرحبا بالعالم!',
+                            'Chinese': '你好世界！',
+                            'English': 'Hello World!',
+                            'French': 'Bonjour monde!',
+                            'German': 'Hallo Welt!',
+                            'Persian': 'سلام دنیا!',
+                            'Russian': 'Привет мир!',
+                            'Catalan': 'Hola món!',
+                            'Hebrew': 'שלום עולם!',
+                            'Spanish': 'Hola!',
+                            'Latin': 'Salve!'}
+
+while True:
+    print("What language do you know?")
+    print("Option:\n"
+          "- Arabic \n"
+          "- Chinese\n"
+          "- English\n"
+          "- French\n"
+          "- German\n"
+          "- Persian\n"
+          "- Russian\n"
+          "- Catalan\n"
+          "- Hebrew\n"
+          "- Spanish\n"
+          "- Latin\n")
+    language = input()
+    if language in hello_world_translations.keys():
+        print(hello_world_translations[language])
+        break
+    else:
+        print("We don't support this languange yet\n"
+              "\n"
+              "　　　　　🌸＞＿＿ フ\n"
+              "　　　　　| 　_　 _ l\n"
+              "　 　　　／` ミ＿xノ\n"
+              "　　 　 /　　　 　 |\n"
+              "　　　 /　 ヽ　　 ﾉ\n"
+              "　 　 │　　|　|　|\n"
+              "　／￣|　　 |　|　|\n"
+              "　| (￣ヽ＿_ヽ_)__)\n"
+              "　＼二つ")
+
+
 while True:
     print("What is your mood today?")
     print("Option:\n"
